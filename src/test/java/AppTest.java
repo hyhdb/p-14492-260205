937baeebf7bb91e5bed4f51ce9473e11eb3f2b1e
mw1126 <kkminwoo@gmail.com>
2026-02-06 11:45:27 +0900

    9강 red

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -17,4 +17,19 @@ public class AppTest {
 
     }
 
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String out = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """
+        );
+        assertThat(out).contains("명령) ");
+        assertThat(out).contains("명언: ");
+        assertThat(out).contains("작가: ");
+
+    }
 }
